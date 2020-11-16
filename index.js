35d6d446cbd804061df3542173cf4f54151c3a0b
Brage Rosberg <brage70@gmail.com>
2020-11-16 14:55:16 +0100

    Verification route setup and working

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { JsonDB } = require('node-json-db');
 const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 
 const app = express();
+app.use(express.json());
 
 const db = new JsonDB(new Config('2faDB', true, false, '/'));
 
@@ -28,6 +29,33 @@ app.post('/api/register', (req, res) => {
   }
 });
 
+// Verify token and make secret perm
+app.post('/api/verify', (req, res) => {
+  const { token, userId } = req.body;
+
+  try {
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+
+    const { base32:secret } = user.tempSecret;
+
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token
+      });
+
+    if(verified) {
+      db.push(path, { id: userId, secret: user.tempSecret });
+      res.json({ verified: true });
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving user'});
+  }
+})
 
 
 
